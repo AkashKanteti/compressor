ac7f7759faa1f5646bd3b8618092955006d5d963
AkashKanteti <akashkanteti@gmail.com>
2024-01-16 11:28:54 +0530

    added test for tree heap

--- binary_tree.go
+++ binary_tree.go
@@ -1,36 +1,38 @@
 package main
 
 type node struct {
-	isLeaf bool
-	val    int
+	isLeaf    bool
+	frequency int
+	val       rune
 }
 
 type tree struct {
-	root      node
-	leftnode  node
-	rightnode node
+	root      *node
+	leftnode  *node
+	rightnode *node
 }
 
 type treeHeap []tree
 
-func (n *treeHeap) Len() int {
-	return len(*n)
+func (n treeHeap) Len() int {
+	return len(n)
 }
 
-func (n *treeHeap) Less(i, j tree) bool {
-	if i.root.val > j.root.val {
-		return true
-	}
-	return false
+func (n treeHeap) Less(i, j int) bool {
+	return n[i].root.frequency > n[j].root.frequency
 }
 
-func (n *treeHeap) Swap(i, j tree) {
-	i.root.val, j.root.val = j.root.val, i.root.val
-	i.root.isLeaf, j.root.isLeaf = j.root.isLeaf, i.root.isLeaf
+func (n treeHeap) Swap(i, j int) {
+	n[i].root, n[j].root = n[j].root, n[i].root
+	n[i].leftnode, n[j].leftnode = n[j].leftnode, n[i].leftnode
+	n[i].rightnode, n[j].rightnode = n[j].rightnode, n[i].rightnode
+	// n[i].root.frequency, n[j].root.frequency = n[j].root.frequency, n[i].root.frequency
+	// n[i].root.isLeaf, n[j].root.isLeaf = n[j].root.isLeaf, n[i].root.isLeaf
+	// n[i].root.val, n[j].root.val = n[j].root.val, n[i].root.val
 }
 
-func (n *treeHeap) Push(x tree) {
-	*n = append(*n, x)
+func (n *treeHeap) Push(x any) {
+	*n = append(*n, x.(tree))
 }
 
 func (n *treeHeap) Pop() any {
--- main_test.go
+++ main_test.go
@@ -1,14 +1,41 @@
 package main
 
 import (
+	"container/heap"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TextParsing(t *testing.T) {
+func TestParsing(t *testing.T) {
 	str := "all is well"
 	expectedMap := map[rune]int{'a': 1, 'l': 4, ' ': 2, 'i': 1, 's': 1, 'w': 1, 'e': 1}
 	actualMap := parsing(str)
 	assert.Equal(t, expectedMap, actualMap)
+}
+
+func TestHeap(t *testing.T) {
+	th := &treeHeap{}
+	heap.Init(th)
+
+	for i := 1; i <= 10; i++ {
+		root1 := node{
+			val:       1,
+			frequency: i,
+			isLeaf:    true,
+		}
+		tree1 := tree{
+			root: &root1,
+		}
+		heap.Push(th, tree1)
+	}
+	expectedList := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	actualList := []int{}
+	for th.Len() > 0 {
+		t := heap.Pop(th)
+		tree := t.(tree)
+		actualList = append(actualList, tree.root.frequency)
+	}
+
+	assert.Equal(t, expectedList, actualList)
 }
